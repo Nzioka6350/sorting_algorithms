ccafd87c093a8483522006ec0609b587b0b4e01e
Nzioka6350 <jamesnziokamutua1@gmail.com>
2023-10-24 15:12:15 -0700

    Moving

--- sort.h
+++ sort.h
@@ -24,5 +24,7 @@ void insertion_sort_list(listint_t **list);
 void selection_sort(int *array, size_t size);
 void quick_sort(int *array, size_t size);
 void shell_sort(int *array, size_t size);
+int get_max(int *array, size_t size);
+void counting_sort(int *array, size_t size);
 
 #endif/* _SORT_H_ */
